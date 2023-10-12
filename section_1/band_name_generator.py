847efe423381b74698b4547c5a1213d00de944df
Princess Connie Tamayao <tammytamayaogmail.com@Princesss-MacBook-Pro.local>
2023-10-12 13:00:07 +0800

    section 1 - band name generator

--- section_1/band_name_generator.py
+++ section_1/band_name_generator.py
@@ -1,4 +1,7 @@
 print("Welcome to Band Name Generator");
+# get user input for city
 city = input("What's name of the city you grew up in?\n");
+# get user input for pet name
 pet_name = input("What's your pet's name?\n");
+# combine city and name to create band name
 print(f"Your band name could be {city} {pet_name}");
