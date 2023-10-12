3fa616de3200ca8e9ddc24fa5a0174ea12f6cfd7
Princess Connie Tamayao <tammytamayaogmail.com@Princesss-MacBook-Pro.local>
2023-10-12 14:55:38 +0800

    section 2 - tip calculator

--- section_2/tip_calculator.py
+++ section_2/tip_calculator.py
@@ -1,7 +1,11 @@
 print("Welcome to the Tip Calculator");
+# get user input for total bill
 total_bill = input("What was the total bill? $");
+# get user input for tip amount
 percentage_tip = input("What percentage tip would you like to give? 10, 12, or 15? ");
+# determine number of people included for bill split
 num_of_people = input("How many people to split the bill? ");
-
+# calculating bill per person
 bill_per_person = (float(total_bill) * (1+ float(percentage_tip)/100))/int(num_of_people)
+# print output
 print(f"Each person should pay ${round(float(bill_per_person),2)}");
